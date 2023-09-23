7399caa2e8c707736247333756d158c9c9735e05
Murat Can <muratal0606@gmail.com>
2023-09-24 00:49:39 +0300

    remove default files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Text fontSize="40px">Clone Story Estimate</Text>
+    </ChakraProvider>
+  );
+}
+
+export default App;
