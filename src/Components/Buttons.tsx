718c9e4ea7397a85c7a3b52852e9a2918da18ed1
Murat Can <muratal0606@gmail.com>
2023-09-25 19:32:32 +0300

    give style to buttons

--- src/Components/Buttons.tsx
+++ src/Components/Buttons.tsx
@@ -2,9 +2,24 @@ import { Button, Flex } from "@chakra-ui/react";
 
 const Buttons = () => {
   return (
-    <Flex m="30px 10px auto 10px">
-      <Button bg="#44cb70">Button</Button>
-      <Button bg="#6c757d">Button</Button>
+    <Flex m="30px 10px auto 10px" p="5px">
+      <Button
+        bg="#44cb70"
+        h="35px"
+        mr="auto"
+        textColor="white"
+        _hover={{ bg: "#36db70" }}
+      >
+        Reveal Votes
+      </Button>
+      <Button
+        bg="#6c757d"
+        h="35px"
+        textColor="white"
+        _hover={{ bg: "#585f69" }}
+      >
+        Reset Estimation
+      </Button>
     </Flex>
   );
 };
