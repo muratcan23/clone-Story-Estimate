873adbab51cb48bf4781d600f038db96524e612e
Murat Can <muratal0606@gmail.com>
2023-09-28 00:56:20 +0300

    take username and vote

--- src/Components/Votes.tsx
+++ src/Components/Votes.tsx
@@ -1,18 +1,93 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Button,
+  Flex,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useState } from "react";
+
 const POINTS = [0, 1, 2, 3, 4, 5, 8, 13, "?"];
 
 type VoteProps = {};
 const Votes: React.FC<VoteProps> = () => {
   const [vote, setVote] = useState<string | null>("");
+  const [username, setUsername] = useState<string>("");
+  const [voteEntries, setVoteEntries] = useState<
+    { username: string; vote: string }[]
+  >([]);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleButtonClick = (buttonValue: string | number) => {
     setVote(buttonValue.toString());
-    console.log(vote);
+    onOpen();
+  };
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleVoteEntry = () => {
+    const enteredUsername = username.trim();
+
+    if (enteredUsername !== "") {
+      setVoteEntries((prevEntries) => [
+        ...prevEntries,
+        { username: enteredUsername, vote: vote || "" },
+      ]);
+
+      setUsername("");
+      onClose();
+    } else {
+      onOpen();
+    }
   };
 
   return (
     <Flex m="15px auto auto 10px">
+      {
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalBody>
+              <Alert
+                status="error"
+                variant="subtle"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                textAlign="center"
+                height="100%"
+              >
+                <AlertIcon boxSize="40px" mr={0} />
+                <AlertTitle m="5px" fontSize="18px">
+                  You must enter a valid username
+                </AlertTitle>
+                <Input
+                  bg="white"
+                  placeholder="Enter username plz"
+                  value={username}
+                  onChange={handleUsernameChange}
+                  m="4px"
+                />
+                <Button colorScheme="green" onClick={handleVoteEntry} mt="5px">
+                  Submit
+                </Button>
+              </Alert>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      }
       {POINTS.map((point) => (
         <Box
           key={point}
@@ -32,6 +107,24 @@ const Votes: React.FC<VoteProps> = () => {
           </Text>
         </Box>
       ))}
+      {isOpen && (
+        <Alert status="error" mt="15px" width="100%">
+          <AlertIcon />
+          <AlertTitle>Error!</AlertTitle>
+          <AlertDescription>Please enter a valid username.</AlertDescription>
+        </Alert>
+      )}
+
+      {/* tempororay  displaying entered votes */}
+      <Flex ml="auto" mr="10px">
+        <Box bg="tomato">
+          {voteEntries.map((entry, index) => (
+            <li key={index}>
+              {entry.username}= {entry.vote}
+            </li>
+          ))}
+        </Box>
+      </Flex>
     </Flex>
   );
 };
